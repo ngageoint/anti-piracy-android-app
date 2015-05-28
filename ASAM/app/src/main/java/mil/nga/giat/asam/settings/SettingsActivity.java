efda1e086bef5e5da3cf377ecd6fd43976cf844f
William Newman <newmanw10@gmail.com>
2015-05-28 15:12:26 -0600

    Settings as ListView
    Using a listview allows state activated

--- ASAM/app/src/main/java/mil/nga/giat/asam/settings/SettingsActivity.java
+++ ASAM/app/src/main/java/mil/nga/giat/asam/settings/SettingsActivity.java
@@ -1,16 +1,12 @@
 package mil.nga.giat.asam.settings;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 
-import mil.nga.giat.asam.PullAsamsTask;
 import mil.nga.giat.asam.R;
 import mil.nga.giat.asam.settings.about.AboutActivity;
 import mil.nga.giat.asam.settings.about.AboutFragment;
@@ -20,15 +16,13 @@ import mil.nga.giat.asam.settings.opensource.OpenSourceActivity;
 import mil.nga.giat.asam.settings.opensource.OpenSourceFragment;
 import mil.nga.giat.asam.settings.privacy.PrivacyActivity;
 import mil.nga.giat.asam.settings.privacy.PrivacyFragment;
-import mil.nga.giat.asam.util.SyncTime;
 
 
-public class SettingsActivity extends AppCompatActivity implements SettingsFragment.InfoClickListener, PullAsamsTask.OnSyncCompletedListener {
+public class SettingsActivity extends AppCompatActivity implements SettingsFragment.SettingClickListener {
 
     private SettingsFragment settingsFragment = null;
     private View detailContainer = null;
     private Fragment detailFragment = null;
-    private TextView syncDate = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,12 +30,9 @@ public class SettingsActivity extends AppCompatActivity implements SettingsFragm
         setContentView(R.layout.settings_info);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        syncDate = (TextView) findViewById(R.id.sync_date);
-        syncDate.setText(SyncTime.getLastSyncTimeAsText(this));
-
         detailContainer = findViewById(R.id.detail_container);
         if (detailContainer != null) {
-            settingsFragment.selectFirstRow();
+            settingsFragment.selectSetting(0);
         }
     }
 
@@ -117,42 +108,5 @@ public class SettingsActivity extends AppCompatActivity implements SettingsFragm
                     .replace(detailContainer.getId(), detailFragment)
                     .commit();
         }
-    }
-
-    @Override
-    public void onSyncClick() {
-        if (SyncTime.isSynched(this)) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AppCompatAlertDialogStyle);
-            builder.setTitle(getString(R.string.preferences_no_sync_title_text));
-            builder.setMessage(getString(R.string.preferences_no_sync_description_text));
-            builder.setPositiveButton(getString(R.string.preferences_ok_button_text), new DialogInterface.OnClickListener() {
-
-                @Override
-                public void onClick(DialogInterface dialog, int id) {
-                    dialog.cancel();
-                }
-            });
-            builder.show();
-            return;
-        }
-
-        new PullAsamsTask(this, this).execute();
-    }
-
-    @Override
-    public void onSyncCompleted() {
-        syncDate.setText(SyncTime.getLastSyncTimeAsText(this));
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AppCompatAlertDialogStyle);
-        builder.setTitle(getString(R.string.preferences_sync_complete_title_text));
-        builder.setMessage(getString(R.string.preferences_sync_complete_description_text));
-        builder.setPositiveButton(getString(R.string.preferences_ok_button_text), new DialogInterface.OnClickListener() {
-
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                dialog.cancel();
-            }
-        });
-        builder.show();
     }
 }
--- ASAM/app/src/main/java/mil/nga/giat/asam/settings/SettingsFragment.java
+++ ASAM/app/src/main/java/mil/nga/giat/asam/settings/SettingsFragment.java
@@ -1,28 +1,36 @@
 package mil.nga.giat.asam.settings;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import mil.nga.giat.asam.PullAsamsTask;
 import mil.nga.giat.asam.R;
+import mil.nga.giat.asam.util.SyncTime;
 
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements AdapterView.OnItemClickListener, PullAsamsTask.OnSyncCompletedListener {
 
-    private View aboutRow;
+    private ListView listView;
+    private ListAdapter listAdapter;
 
-    public interface InfoClickListener {
+    public interface SettingClickListener {
         void onAboutClick();
         void onDisclaimerClick();
         void onPrivacyPolicyClick();
         void onOpenSourceClick();
-        void onSyncClick();
     }
 
-    private InfoClickListener infoClickListener;
+    private SettingClickListener settingClickListener;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,41 +41,11 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.settings_info_fragment, container, false);
 
-        aboutRow = view.findViewById(R.id.about_row);
-        aboutRow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                infoClickListener.onAboutClick();
-            }
-        });
+        listView = (ListView) view.findViewById(R.id.list_view);
+        listAdapter = new SettingsAdapter(view.getContext());
+        listView.setAdapter(listAdapter);
+        listView.setOnItemClickListener(this);
 
-        view.findViewById(R.id.disclaimer_row).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                infoClickListener.onDisclaimerClick();
-            }
-        });
-
-        view.findViewById(R.id.sync_row).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                infoClickListener.onSyncClick();
-            }
-        });
-
-        view.findViewById(R.id.privacy_policy_row).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                infoClickListener.onPrivacyPolicyClick();
-            }
-        });
-
-        view.findViewById(R.id.open_source_row).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                infoClickListener.onOpenSourceClick();
-            }
-        });
         return view;
     }
 
@@ -76,13 +54,72 @@ public class SettingsFragment extends Fragment {
         super.onAttach(activity);
 
         try {
-            infoClickListener = (InfoClickListener) activity;
+            settingClickListener = (SettingClickListener) activity;
         } catch (ClassCastException caught) {
             throw new ClassCastException(activity.toString() + " must implement InfoClickListener");
         }
     }
 
-    public void selectFirstRow() {
-        aboutRow.performClick();
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 0:
+                settingClickListener.onAboutClick();
+                break;
+            case 1:
+                settingClickListener.onDisclaimerClick();
+                break;
+            case 2:
+                settingClickListener.onPrivacyPolicyClick();
+                break;
+            case 3:
+                settingClickListener.onOpenSourceClick();
+                break;
+            case 4:
+                onSyncClick();
+                break;
+        }
+    }
+
+    public void selectSetting(int position) {
+        listView.performItemClick(listView, position, listView.getItemIdAtPosition(position));
+    }
+
+    @Override
+    public void onSyncCompleted() {
+        View v = listView.getChildAt(4);
+        TextView date = (TextView) v.findViewById(R.id.title);
+        date.setText(String.format(getActivity().getString(R.string.preferences_last_sync_time_label_text), SyncTime.getLastSyncTimeAsText(getActivity())));
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.AppCompatAlertDialogStyle);
+        builder.setTitle(getString(R.string.preferences_sync_complete_title_text));
+        builder.setMessage(getString(R.string.preferences_sync_complete_description_text));
+        builder.setPositiveButton(getString(R.string.preferences_ok_button_text), new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    }
+
+    private void onSyncClick() {
+        if (SyncTime.isSynched(getActivity())) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.AppCompatAlertDialogStyle);
+            builder.setTitle(getString(R.string.preferences_no_sync_title_text));
+            builder.setMessage(getString(R.string.preferences_no_sync_description_text));
+            builder.setPositiveButton(getString(R.string.preferences_ok_button_text), new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                }
+            });
+            builder.show();
+            return;
+        }
+
+        new PullAsamsTask(getActivity(), this).execute();
     }
 }
