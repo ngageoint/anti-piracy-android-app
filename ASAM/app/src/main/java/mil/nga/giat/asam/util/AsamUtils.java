c9c0b2fc3adb175f86b1318ba8e0a40d08c6160c
Christopher Caldwell <ccaldwell@caci.com>
2017-03-02 09:40:17 -0500

    cleanup

--- ASAM/app/src/main/java/mil/nga/giat/asam/util/AsamUtils.java
+++ ASAM/app/src/main/java/mil/nga/giat/asam/util/AsamUtils.java
@@ -2,21 +2,11 @@ package mil.nga.giat.asam.util;
 
 import android.content.Context;
 import android.content.res.Configuration;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.drawable.BitmapDrawable;
-import android.util.TypedValue;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
-import mil.nga.giat.asam.R;
 
 
 public class AsamUtils {
