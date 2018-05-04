095e3bea170423306888475fd1f30a20935eabf8
William Newman <newmanw10@gmail.com>
2018-05-04 10:56:31 -0600

    Add IdenTrust ROOT CA
    * Also upgraded gradle build as well as pulled in OkHttp in favor of deprecated apache http libraries.

--- ASAM/app/src/main/java/mil/nga/giat/asam/net/AsamWebService.java
+++ ASAM/app/src/main/java/mil/nga/giat/asam/net/AsamWebService.java
@@ -1,29 +1,23 @@
 package mil.nga.giat.asam.net;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 import mil.nga.giat.asam.db.AsamDbHelper;
 import mil.nga.giat.asam.util.AsamConstants;
 import mil.nga.giat.asam.util.AsamLog;
 import mil.nga.giat.asam.util.AsamUtils;
 import mil.nga.giat.asam.util.ManifestMetaData;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class AsamWebService {
 
@@ -34,28 +28,38 @@ public class AsamWebService {
     public AsamWebService(Context context) {
         mContext = context;
     }
+
+    private static OkHttpClient httpClient;
+
+    static {
+        httpClient = new OkHttpClient.Builder()
+                .connectTimeout(AsamConstants.QUERY_TIMEOUT_THRESHOLD_IN_MILLISECONDS, TimeUnit.MILLISECONDS)
+                .readTimeout(AsamConstants.QUERY_TIMEOUT_THRESHOLD_IN_MILLISECONDS, TimeUnit.MILLISECONDS)
+                .build();
+    }
     
     public String query() throws IOException {
         String results = null;
-        Date maxDate = null;
         AsamDbHelper dbHelper = new AsamDbHelper(mContext);
         SQLiteDatabase db = dbHelper.getReadableDatabase();
-        maxDate = dbHelper.getMaxOccurrenceDate(db);
+        Date maxDate = dbHelper.getMaxOccurrenceDate(db);
         db.close();
         
         String url = String.format(ManifestMetaData.getString(mContext, "web_service_url"), DATE_FORMAT.format(maxDate), DATE_FORMAT.format(new Date()));
         AsamLog.i(AsamWebService.class.getName() + ":" + url);
-        HttpParams httpParameters = new BasicHttpParams();
-        HttpConnectionParams.setConnectionTimeout(httpParameters, AsamConstants.QUERY_TIMEOUT_THRESHOLD_IN_MILLISECONDS);
-        HttpConnectionParams.setSoTimeout(httpParameters, AsamConstants.QUERY_TIMEOUT_THRESHOLD_IN_MILLISECONDS);
-        HttpClient httpClient = new DefaultHttpClient(httpParameters);
-        HttpResponse response = httpClient.execute(new HttpGet(url));
-        HttpEntity entity = response.getEntity();
-        if (entity != null) {
-            InputStream in = entity.getContent();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = httpClient.newCall(request).execute();
+
+        if (response != null && response.isSuccessful()) {
+            InputStream in = response.body().byteStream();
             results = AsamUtils.readStream(in);
             in.close();
         }
+
         return results;
     }
 }
